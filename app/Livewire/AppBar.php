54048df7bf2dbcbad9a9322020d8be1cac5d321f
renz <dev.renzsalanga@gmail.com>
2025-04-13 15:44:16 +0800

    closes #10

--- app/Livewire/AppBar.php
+++ app/Livewire/AppBar.php
@@ -3,28 +3,9 @@
 namespace App\Livewire;
 
 use Livewire\Component;
-use Livewire\Attributes\On;
 
 class AppBar extends Component
 {
-    public $isDrawerOpen = false;
-
-    #[On('icon-button-click')]
-    public function handleIconButtonClick(string $action)
-    {
-        switch ($action) {
-            case 'open-drawer':
-                $this->toggleDrawer();
-                break;
-            default:
-        }
-    }
-
-    private function toggleDrawer()
-    {
-        $this->isDrawerOpen = !$this->isDrawerOpen;
-    }
-
     public function render()
     {
         return view('livewire.app-bar');
