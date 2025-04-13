106d0142efdffa69fa04756ca90e4e13bf83ee20
renz <dev.renzsalanga@gmail.com>
2025-04-13 10:39:00 +0800

    refactor: added scaffold component for easier building new page

---
+++ resources/views/components/scaffold.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>{{ config('app.name') }}</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @livewireStyles
+    @vite(['resources/js/app.js', 'resources/css/app.css'])
+</head>
+<body>
+    {{ $slot }}
+
+    @livewireScriptConfig
+</body>
+</html>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,15 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <title>Document</title>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    @livewireStyles
-    @vite(['resources/js/app.js', 'resources/css/app.css'])
-</head>
-<body>
+<x-scaffold>
     <livewire:main-layout />
-
-    @livewireScriptConfig
-</body>
-</html>
+</x-scaffold>
