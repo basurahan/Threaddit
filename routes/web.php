3a62303fbfb861a7b4aa47b2bffb36c6d47043e4
renz <dev.renzsalanga@gmail.com>
2025-05-05 22:25:44 +0800

    wip: login (added initial login)

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\View\View;
 use App\Http\Controllers\HomeController;
 
 Route::get('/', function() {
     return redirect()->route('home', ['section' => 'home']);
+});
+Route::get('/login', function() {
+    return view('livewire.pages.login');
 });
 Route::get('/{section}', HomeController::class)->name('home');
